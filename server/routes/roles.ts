640fc9d7c7e659dc983d8aacc920b74db25ed56b
Alvaro Sakae <asakaef@users.noreply.github.com>
2026-07-02 15:59:56 +0000

    hardening: restrict roles endpoint to role admins

--- server/routes/roles.ts
+++ server/routes/roles.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
 import { dbStore } from "../../src/dbStore";
-import { requireAuth } from "./auth";
+import { requirePermission } from "./auth";
 
 const router = express.Router();
 
-router.get("/", requireAuth, (req: Request, res: Response, next: NextFunction) => {
+router.get("/", requirePermission("admin:roles"), (req: Request, res: Response, next: NextFunction) => {
   try {
     res.json(dbStore.getData().roles);
   } catch (err) {
